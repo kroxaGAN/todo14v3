a92dde3e9de110abef021c2df1a79405acca1d35
Andrei Hutsau <mwplt@mail.ru>
2023-01-05 23:46:52 +0300

    2 skills

---
+++ ITNEW/MYCV/cv/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import {Header} from "./header/Header";
+import {Main} from "./main/Main";
+import {Skills} from "./skills/Skills";
+
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <Main/>
+            <Skills/>
+            <Skills/>
+        </div>
+    );
+}
+
+export default App;
